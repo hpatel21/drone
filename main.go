3e7910d59d9429d00f36da374d8eec345b5ec804
Harsh Patel <hpatel@C02S41ZXG8WM-M.local>
2017-08-29 17:33:11 -0700

    Drone-Go example

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(HelloWorld())
+}
+
+// HelloWorld function returns Hello World string
+func HelloWorld() string {
+	return "Hello World"
+}
